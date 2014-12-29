4132ef7c7da019cdc463b09ab0cc5fdad2d8bc05
mxyue <basicbox@126.com>
2014-12-29 11:15:27 +0800

    精简测试提交

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,10 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title" do
+      visit "/static_pages/home"
+      expect(page).to have_title('Ruby on Rails Tutorail Sample App | Home')
+    end
   end
 
   describe "帮助页面" do
@@ -14,12 +18,20 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title" do
+      visit "/static_pages/help"
+      expect(page).to have_title('Ruby on Rails Tutorail Sample App | Help')
+    end
   end
 
   describe "About page" do
     it "should have the content 'about'"do
       visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+    it "should have the right title" do
+      visit "/static_pages/about"
+      expect(page).to have_title('Ruby on Rails Tutorail Sample App | About')
     end
   end
 end
